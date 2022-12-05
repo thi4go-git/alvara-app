bccb7dba42efafbd60a289981695529fadf4b2d1
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2022-12-05 10:48:51 -0300

    Implementando Rotas Filhas Componente de Preferências

--- src/app/preferencias/preferencias-routing.module.ts
+++ src/app/preferencias/preferencias-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from '../layout/layout.component';
 import { PreferenciasFormComponent } from './preferencias-form/preferencias-form.component';
 
 
 const routes: Routes = [
-  { path: 'preferencias-form', component: PreferenciasFormComponent }
+  {
+    path: 'preferencias', component: LayoutComponent, children: [
+      { path: 'form', component: PreferenciasFormComponent },
+      { path: '', redirectTo: '/preferencias/form', pathMatch: 'full' }
+    ]
+  }
 ];
 
 @NgModule({
