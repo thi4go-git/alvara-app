17feda1d9973ebea70c502769015401ab73bf844
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2022-12-05 12:19:55 -0300

    Implementando Serviço de Autenticação

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,12 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+const API_URL_BASE = 'http://localhost:8080';
+const API_URL_TOKEN = '/oauth/token';
+const CLI_ID = 'my-angular-app';
+const CLI_SECRET = '@321';
 
-const API_URL = 'http://localhost:8080/....';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -14,5 +18,21 @@ const httpOptions = {
 export class AuthService {
 
   constructor(private http: HttpClient) { }
+
+
+  obterToken(username: string, password: string): Observable<any> {
+    const params = new HttpParams()
+      .set('username', username)
+      .set('password', password)
+      .set('grant_type', 'password');
+
+    const headers = {
+      'Authorization': 'Basic ' + btoa(CLI_ID + ':' + CLI_SECRET),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    };
+
+    return this.http.post(API_URL_BASE + API_URL_TOKEN, params.toString(), { headers });
+  }
+
 
 }
