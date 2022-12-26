80292f17a96e1e89cf804bd8a698727ebb45a89d
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2022-12-26 13:48:59 -0300

    Msg erro login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -23,13 +23,20 @@ export class LoginComponent {
   onSubmit() {
     this.authService
       .obterToken(this.username, this.password)
-      .subscribe(response => {    
+      .subscribe(response => {
         const access_token = JSON.stringify(response);
         localStorage.setItem('access_token', access_token);
         this.router.navigate(['/home/inicio'])
       }, errorResponse => {
+        const status = errorResponse.status;
+        const msgErro = errorResponse.message;
         this.loginError = true;
-        this.erros = [errorResponse.error.error_description];
+        if (status == 0) {
+          const infoErr = 'STATUS: (' + status + ") " + msgErro;
+          this.erros = [infoErr];
+        } else {
+          this.erros = [errorResponse.error.error_description];
+        }
       }
       )
   }
